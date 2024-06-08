7e89c60c753ef30cb2b96673a51bcd20c6bc97e0
ahadsenja <hamdanilm96@gmail.com>
2024-06-09 02:28:37 +0800

    update footer

--- src/App.tsx
+++ src/App.tsx
@@ -22,42 +22,46 @@ function App() {
   }
 
   return (
-    <main className='flex flex-col justify-center items-center min-h-[80vh] w-full max-w-3xl mx-auto space-y-4'>
-      <div>
-        <h1 className='flex justify-center items-center text-xl sm:text-3xl text-indigo-500'>Regroq AI</h1>
-        <span className='text-gray-300 text-xs sm:text-sm'>Type anything you want, and see the result</span>
-      </div>
+    <>
+      <main className='flex flex-col justify-center items-center min-h-[80vh] w-full max-w-3xl mx-auto space-y-4'>
+        <div>
+          <h1 className='flex justify-center items-center text-xl sm:text-3xl text-indigo-500'>Regroq AI</h1>
+          <span className='text-gray-300 text-xs sm:text-sm'>Type anything you want, and see the result</span>
+        </div>
 
-      <form action="" className='flex w-full gap-1'>
-        <input
-          type="text"
-          id='content'
-          placeholder='Type something ...'
-          onKeyDown={handleSubmitWithEnter}
-          className='w-full border border-indigo-500 rounded-md p-2'
-        />
-        <button
-          onClick={handleSubmit}
-          type='button'
-          className='border bg-indigo-500 border-indigo-500 text-white rounded-md p-2'
-        >
-          Run
-        </button>
-      </form>
-
-      <div className='max-w-3xl w-full mx-auto text-sm sm:text-base'>
-        {data ?
-          <SyntaxHighlighter
-            language='swift'
-            style={darcula}
-            wrapLines={true}
+        <form action="" className='flex w-full gap-1'>
+          <input
+            type="text"
+            id='content'
+            placeholder='Type something ...'
+            onKeyDown={handleSubmitWithEnter}
+            className='w-full border border-indigo-500 rounded-md p-2'
+          />
+          <button
+            onClick={handleSubmit}
+            type='button'
+            className='border bg-indigo-500 border-indigo-500 text-white rounded-md p-2'
           >
-            {data}
-          </SyntaxHighlighter> 
-          : null
-        }
-      </div>
-    </main>
+            Run
+          </button>
+        </form>
+
+        <div className='max-w-3xl w-full mx-auto text-sm sm:text-base'>
+          {data ?
+            <SyntaxHighlighter
+              language='swift'
+              style={darcula}
+              wrapLines={true}
+            >
+              {data}
+            </SyntaxHighlighter>
+            : null
+          }
+        </div>
+      </main>
+
+      <footer className='w-full text-xs text-center px-2 text-gray-300 fixed bottom-2 left-0'>Copyright &copy; 2024 Regroq AI. All rights reserved. Made by <a href="https://github.com/ahadsenja" target="_blank" rel="noreferrer" className='font-bold'>@ahadsenja</a></footer>
+    </>
   )
 }
 
